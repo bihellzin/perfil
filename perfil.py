849c044e38897a7831fedb4c032aa4d17fff6ac9
bihellzinho <gof2@cin.ufpe.br>
2019-09-23 23:26:52 -0300

    adding new methods

--- perfil.py
+++ perfil.py
@@ -3,21 +3,20 @@ import numpy as np
 
 class Perfil:
     def __init__(self):
-        probabilidade = np.array(
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+        probabilidade = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
              27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
              52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76,
              77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100])
         self.estilo = self.definindoEstilo()
         self.genero = self.definindoGenero(probabilidade)
         self.idade = self.definindoIdade(probabilidade)
-        self.estado_civil = None
-        self.necessidades = None
-        self.pessoas = None
-        self.animais = None
-        self.altura = None
+        self.estado_civil = self.definindoEstadoCivil(probabilidade)
+        self.necessidades = self.definindoNecessidades(probabilidade)
+        self.pessoas = self.definindoPessoas(probabilidade)
+        self.animais = self.definindoAnimais(probabilidade)
+        self.altura = self.definindoAltura(probabilidade)
         self.hobbies = self.definindoHobbies()
-        self.espaco = None
+        self.espaco = self.definindoEspaco()
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -74,3 +73,35 @@ class Perfil:
         h2 = hobbies.pop(random.randint(0, 34))
         h3 = hobbies.pop(random.randint(0, 33))
         return h1 + ', ' + h2 + ' e ' + h3
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def definindoEspaco(self):
+        espacos = self.leitura("espaco_mais.txt")
+        espaco = random.choice(espacos)
+        return espaco
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def definindoEstadoCivil(self, probabilidade):
+        pass
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def definindoNecessidades(self, probabilidade):
+        pass
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def definindoPessoas(self, probabilidade):
+        pass
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def definindoAnimais(self, probabilidade):
+        pass
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def definindoAltura(self, probabilidade):
+        pass
